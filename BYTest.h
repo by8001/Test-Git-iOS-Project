e42294e356c2a6736d067b6a76c1d5686dec2e9b
Rachel Berry <raberry08@Rachel-Berrys-MacBook-Pro.local>
2014-03-02 21:15:27 -0500

    added a test class

---
+++ BYTest.h
@@ -0,0 +1,13 @@
+//
+//  BYTest.h
+//  Git Test
+//
+//  Created by Rachel Berry on 3/2/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BYTest : NSObject
+
+@end
---
+++ BYTest.m
@@ -0,0 +1,13 @@
+//
+//  BYTest.m
+//  Git Test
+//
+//  Created by Rachel Berry on 3/2/14.
+//
+//
+
+#import "BYTest.h"
+
+@implementation BYTest
+
+@end
